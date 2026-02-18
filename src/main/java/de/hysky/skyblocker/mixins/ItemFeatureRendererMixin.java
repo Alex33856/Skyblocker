2954b5a3b9911fc952a0213f3e83fa1f02e3f9fb
alex <8379108+Alex33856@users.noreply.github.com>
2026-02-18 17:54:50 -0500

    26.1: fix todos

--- src/main/java/de/hysky/skyblocker/mixins/ItemFeatureRendererMixin.java
+++ src/main/java/de/hysky/skyblocker/mixins/ItemFeatureRendererMixin.java
@@ -1,35 +1,38 @@
 package de.hysky.skyblocker.mixins;
 
-import org.spongepowered.asm.mixin.Mixin;
-//? if <= 1.21.11 {
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-
 import de.hysky.skyblocker.skyblock.entity.MobGlow;
 import de.hysky.skyblocker.utils.render.GlowRenderer;
 import net.minecraft.client.renderer.OutlineBufferSource;
 import net.minecraft.client.renderer.SubmitNodeStorage;
-//? }
 import net.minecraft.client.renderer.feature.ItemFeatureRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ItemFeatureRenderer.class)
 public class ItemFeatureRendererMixin {
-	// TODO (26.1): Fix
-	//? if <= 1.21.11 {
-
-	@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/SubmitNodeStorage$ItemSubmit;outlineColor()I"), require = 2)
+	@WrapOperation(
+			//? if >1.21.11 {
+			/*method = {"renderSolid", "renderTranslucent"},
+			*///? } else {
+			method = "render",
+			//? }
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/SubmitNodeStorage$ItemSubmit;outlineColor()I"), require = 2)
 	private int skyblocker$useCustomGlowColour(SubmitNodeStorage.ItemSubmit command, Operation<Integer> operation) {
 		return command.skyblocker$getCustomGlowColour() != MobGlow.NO_GLOW ? command.skyblocker$getCustomGlowColour() : operation.call(command);
 	}
 
-	@ModifyVariable(method = "render", at = @At("LOAD"), argsOnly = true, require = 2)
+	@ModifyVariable(
+			//? if >1.21.11 {
+			/*method = {"renderSolid", "renderTranslucent"},
+			*///? } else {
+			method = "render",
+			//? }
+			at = @At("LOAD"), argsOnly = true, require = 2)
 	private OutlineBufferSource skyblocker$useCustomGlowConsumers(OutlineBufferSource original, @Local SubmitNodeStorage.ItemSubmit command) {
 		return command.skyblocker$getCustomGlowColour() != MobGlow.NO_GLOW ? GlowRenderer.getInstance().getGlowVertexConsumers() : original;
 	}
-
-	//? }
 }
